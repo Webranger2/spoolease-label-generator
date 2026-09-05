28bfa3286ac7d31b22c6a463727f8a581679c227
Danyel <44174579+Webranger2@users.noreply.github.com>
2026-09-05 20:41:12 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'spoolease-cache-v3';
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
